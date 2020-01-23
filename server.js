674e57b2d17f74f714554cd15ebd4a6efc30dcc7
Cinth010 <cinthia.rbs010@gmail.com>
2020-01-23 18:45:39 -0300

    config do heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.options('*', cors());
 app.use('/', router);
-
-app.listen(3000), () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT), () => {
     console.log("App Online");
 }
